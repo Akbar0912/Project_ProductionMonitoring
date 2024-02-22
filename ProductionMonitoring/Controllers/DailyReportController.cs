0eb3ac64b30cc54a24113b662a754bc71632e18b
Akbar0912 <ap411342@gmail.com>
2024-02-22 11:39:36 +0700

    update 22/02/24

---
+++ ProductionMonitoring/Controllers/DailyReportController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductionMonitoring.Controllers
+{
+    public class DailyReportController : Controller
+    {
+        public IActionResult DailyReport()
+        {
+            return View();
+        }
+
+        public IActionResult AddNote() 
+        {
+            return View();
+        }
+
+        public IActionResult DailyMonth() 
+        {
+            return View();
+        }
+
+        public IActionResult AddMonth() 
+        {
+            return View();
+        }
+    }
+}
